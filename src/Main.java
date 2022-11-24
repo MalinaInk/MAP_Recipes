e9180f917e4a43b86951ca33f66e324e5af3a925
mo <nataleshen.ko@yandex.ru>
2022-11-24 20:51:58 +0300

    commit - 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Recipes!");
+        Recipe recipe1 = new Recipe("Банановый коктейль");
+        Product product1 = new Product("Банан", 8);
+        Product product2 = new Product("Молоко", 65);
+        Product product3 = new Product("Корица", 14);
+        recipe1.addProduct(product1, 2);
+        recipe1.addProduct(product2, 1);
+        recipe1.addProduct(product3, 1);
+        System.out.println(recipe1.toString());
+        System.out.println(recipe1.getRecipePrice());
+
     }
 }
